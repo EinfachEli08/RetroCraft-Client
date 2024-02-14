3f07175800bc4e8a9d60f1493d286d1fedc8fdec
Team Omega <einfacheli.games@gmail.com>
2024-02-14 14:29:34 +0100

    Committed run, runserver and src
    this is the Source-Code!

---
+++ src/main/java/net/minecraft/client/gui/screens/TitleScreen.java
@@ -0,0 +1,264 @@
+package net.minecraft.client.gui.screens;
+
+import com.mojang.authlib.minecraft.BanDetails;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.realmsclient.RealmsMainScreen;
+import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import javax.annotation.Nullable;
+import net.minecraft.SharedConstants;
+import net.minecraft.Util;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.components.LogoRenderer;
+import net.minecraft.client.gui.components.MultiLineLabel;
+import net.minecraft.client.gui.components.PlainTextButton;
+import net.minecraft.client.gui.components.SplashRenderer;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraft.client.gui.screens.multiplayer.SafetyScreen;
+import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
+import net.minecraft.client.player.controller.MouseSimulator;
+import net.minecraft.client.renderer.CubeMap;
+import net.minecraft.client.renderer.PanoramaRenderer;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class TitleScreen extends Screen {
+   public static final Component COPYRIGHT_TEXT = Component.literal("Copyright Mojang AB. Do not distribute!");
+   public static final CubeMap CUBE_MAP = new CubeMap(new ResourceLocation("textures/gui/title/background/panorama"));
+   private static final ResourceLocation PANORAMA_OVERLAY = new ResourceLocation("textures/gui/title/background/panorama_overlay.png");
+   @Nullable
+   private SplashRenderer splash;
+   @Nullable
+   private RealmsNotificationsScreen realmsNotificationsScreen;
+   private final PanoramaRenderer panorama = new PanoramaRenderer(CUBE_MAP);
+   private final boolean fading;
+   private long fadeInStart;
+   @Nullable
+   private TitleScreen.WarningLabel warningLabel;
+   private final LogoRenderer logoRenderer;
+
+   public TitleScreen() {
+      this(false);
+   }
+
+   public TitleScreen(boolean p_96733_) {
+      this(p_96733_, (LogoRenderer)null);
+   }
+
+   public TitleScreen(boolean p_265779_, @Nullable LogoRenderer p_265067_) {
+      super(Component.translatable("narrator.screen.title"));
+      this.fading = p_265779_;
+      this.logoRenderer = Objects.requireNonNullElseGet(p_265067_, () -> {
+         return new LogoRenderer(false);
+      });
+   }
+
+   private boolean realmsNotificationsEnabled() {
+      return this.realmsNotificationsScreen != null;
+   }
+
+   public void tick() {
+      if (this.realmsNotificationsEnabled()) {
+         this.realmsNotificationsScreen.tick();
+      }
+
+      this.minecraft.getRealms32BitWarningStatus().showRealms32BitWarningIfNeeded(this);
+   }
+
+   public static CompletableFuture<Void> preloadResources(TextureManager p_96755_, Executor p_96756_) {
+      return CompletableFuture.allOf(p_96755_.preload(LogoRenderer.MINECRAFT_LOGO, p_96756_), p_96755_.preload(LogoRenderer.MINECRAFT_EDITION, p_96756_), p_96755_.preload(PANORAMA_OVERLAY, p_96756_), CUBE_MAP.preload(p_96755_, p_96756_));
+   }
+
+   public boolean isPauseScreen() {
+      return false;
+   }
+
+   public boolean shouldCloseOnEsc() {
+      return false;
+   }
+
+   protected void init() {
+      if (this.splash == null) {
+         this.splash = this.minecraft.getSplashManager().getSplash();
+      }
+
+      int i = this.font.width(COPYRIGHT_TEXT);
+      int j = this.width - i - 2;
+      int l = this.height / 4 + 48;
+      this.createNormalMenuOptions(l, 24);
+      
+
+      this.addRenderableWidget(new ImageButton(this.width / 2 - 124, l + 72 + 12, 20, 20, 0, 106, 20, Button.WIDGETS_LOCATION, 256, 256, (p_280830_) -> {
+         this.minecraft.setScreen(new LanguageSelectScreen(this, this.minecraft.options, this.minecraft.getLanguageManager()));
+      }, Component.translatable("narrator.button.language")));
+      
+      this.addRenderableWidget(Button.builder(Component.translatable("menu.options"), (p_280838_) -> {
+         this.minecraft.setScreen(new OptionsScreen(this, this.minecraft.options));
+      }).bounds(this.width / 2 - 100, l + 72 + 12, 98, 20).build());
+      
+      this.addRenderableWidget(Button.builder(Component.translatable("menu.quit"), (p_280831_) -> {
+         this.minecraft.stop();
+      }).bounds(this.width / 2 + 2, l + 72 + 12, 98, 20).build());
+      
+      this.addRenderableWidget(new ImageButton(this.width / 2 + 104, l + 72 + 12, 20, 20, 0, 0, 20, Button.ACCESSIBILITY_TEXTURE, 32, 64, (p_280835_) -> {
+         this.minecraft.setScreen(new AccessibilityOptionsScreen(this, this.minecraft.options));
+      }, Component.translatable("narrator.button.accessibility")));
+      
+      this.addRenderableWidget(new PlainTextButton(j, this.height - 10, i, 10, COPYRIGHT_TEXT, (p_280834_) -> {
+         this.minecraft.setScreen(new CreditsAndAttributionScreen(this));
+      }, this.font));
+      
+      this.minecraft.setConnectedToRealms(false);
+      
+      if (this.realmsNotificationsScreen == null) {
+         this.realmsNotificationsScreen = new RealmsNotificationsScreen();
+      }
+
+      if (this.realmsNotificationsEnabled()) {
+         this.realmsNotificationsScreen.init(this.minecraft, this.width, this.height);
+      }
+
+      if (!this.minecraft.is64Bit()) {
+         this.warningLabel = new TitleScreen.WarningLabel(this.font, MultiLineLabel.create(this.font, Component.translatable("title.32bit.deprecation"), 350, 2), this.width / 2, l - 24);
+      }
+
+   }
+
+   private void createNormalMenuOptions(int p_96764_, int p_96765_) {
+	   
+      this.addRenderableWidget(Button.builder(Component.translatable("menu.singleplayer"), (p_280832_) -> {
+         this.minecraft.setScreen(new SelectWorldScreen(this));
+      }).bounds(this.width / 2 - 100, p_96764_, 200, 20).build());
+      
+      Component component = this.getMultiplayerDisabledReason();
+      boolean flag = component == null;
+      Tooltip tooltip = component != null ? Tooltip.create(component) : null;
+      
+      (this.addRenderableWidget(Button.builder(Component.translatable("menu.multiplayer"), (p_280833_) -> {
+         Screen screen = (Screen)(this.minecraft.options.skipMultiplayerWarning ? new JoinMultiplayerScreen(this) : new SafetyScreen(this));
+         this.minecraft.setScreen(screen);
+      }).bounds(this.width / 2 - 100, p_96764_ + p_96765_ * 1, 200, 20).tooltip(tooltip).build())).active = flag;
+      
+      (this.addRenderableWidget(Button.builder(Component.translatable("menu.online"), (p_210872_) -> {
+         this.realmsButtonClicked();
+      }).bounds(this.width / 2 - 100, p_96764_ + p_96765_ * 2, 200, 20).tooltip(tooltip).build())).active = flag;
+   }
+
+   @Nullable
+   private Component getMultiplayerDisabledReason() {
+      if (this.minecraft.allowsMultiplayer()) {
+         return null;
+      } else {
+         BanDetails bandetails = this.minecraft.multiplayerBan();
+         if (bandetails != null) {
+            return bandetails.expires() != null ? Component.translatable("title.multiplayer.disabled.banned.temporary") : Component.translatable("title.multiplayer.disabled.banned.permanent");
+         } else {
+            return Component.translatable("title.multiplayer.disabled");
+         }
+      }
+   }
+
+   private void realmsButtonClicked() {
+      this.minecraft.setScreen(new RealmsMainScreen(this));
+   }
+
+   public void render(GuiGraphics gfx, int p_281753_, int p_283539_, float p_282628_) {
+      if (this.fadeInStart == 0L && this.fading) {
+         this.fadeInStart = Util.getMillis();
+      }
+
+      float f = this.fading ? (float)(Util.getMillis() - this.fadeInStart) / 1000.0F : 1.0F;
+      this.panorama.render(p_282628_, Mth.clamp(f, 0.0F, 1.0F));
+      RenderSystem.enableBlend();
+      gfx.setColor(1.0F, 1.0F, 1.0F, this.fading ? (float)Mth.ceil(Mth.clamp(f, 0.0F, 1.0F)) : 1.0F);
+      gfx.blit(PANORAMA_OVERLAY, 0, 0, this.width, this.height, 0.0F, 0.0F, 16, 128, 16, 128);
+      gfx.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+      float f1 = this.fading ? Mth.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
+      
+      this.logoRenderer.renderLogo(gfx, this.width, f1);
+      
+      int i = Mth.ceil(f1 * 255.0F) << 24;
+      if ((i & -67108864) != 0) {
+         if (this.warningLabel != null) {
+            this.warningLabel.render(gfx, i);
+         }
+
+         if (this.splash != null) {
+            this.splash.render(gfx, this.width, this.font, i);
+         }
+
+         String s = "Minecraft " + SharedConstants.getCurrentVersion().getName();
+         if (this.minecraft.isDemo()) {
+            s = s + " Demo";
+         } else {
+            s = s + ("release".equalsIgnoreCase(this.minecraft.getVersionType()) ? "" : "/" + this.minecraft.getVersionType());
+         }
+         
+         gfx.drawString(this.font, s, 2, this.height - 10, 16777215 | i);
+
+         for(GuiEventListener guieventlistener : this.children()) {
+            if (guieventlistener instanceof AbstractWidget) {
+               ((AbstractWidget)guieventlistener).setAlpha(f1);
+            }
+         }
+         
+        
+         
+         super.render(gfx, p_281753_, p_283539_, p_282628_);
+         if (this.realmsNotificationsEnabled() && f1 >= 1.0F) {
+            RenderSystem.enableDepthTest();
+            this.realmsNotificationsScreen.render(gfx, p_281753_, p_283539_, p_282628_);
+         }
+
+      }
+      
+      
+   }
+
+   public boolean mouseClicked(double p_96735_, double p_96736_, int p_96737_) {
+      if (super.mouseClicked(p_96735_, p_96736_, p_96737_)) {
+         return true;
+      } else {
+         return this.realmsNotificationsEnabled() && this.realmsNotificationsScreen.mouseClicked(p_96735_, p_96736_, p_96737_);
+      }
+   }
+
+   public void removed() {
+      if (this.realmsNotificationsScreen != null) {
+         this.realmsNotificationsScreen.removed();
+      }
+
+   }
+
+   public void added() {
+      super.added();
+      if (this.realmsNotificationsScreen != null) {
+         this.realmsNotificationsScreen.added();
+      }
+
+   }
+
+
+   @OnlyIn(Dist.CLIENT)
+   static record WarningLabel(Font font, MultiLineLabel label, int x, int y) {
+      public void render(GuiGraphics p_281783_, int p_281383_) {
+         this.label.renderBackgroundCentered(p_281783_, this.x, this.y, 9, 2, 2097152 | Math.min(p_281383_, 1426063360));
+         this.label.renderCentered(p_281783_, this.x, this.y, 9, 16777215 | p_281383_);
+      }
+   }
+}
