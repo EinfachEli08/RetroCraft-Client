0109ece23a070f4d5fa1fdaecd42407f3f18d049
Team Omega <einfacheli.games@gmail.com>
2024-02-14 20:29:16 +0100

    Added Install.bat and minor tweaks

--- build.bat
+++ build.bat
@@ -1,13 +1,25 @@
 @echo off
 echo MCP-REBORN ASSET INJECTOR
 cd build\libs
-echo rename zip
+
+echo Opening jar
 ren RetroCraft*.jar game.zip
+
 echo injecting assets...
 ..\..\7z.exe a game.zip ..\..\src\main\java\assets\ > log.txt
+
 echo injecting data...
 ..\..\7z.exe a game.zip ..\..\src\main\java\data\ > log.txt
-rem log.txt
-echo rename jar
+del log.txt
+
+echo Closing jar
 ren game.zip RetroCraft.jar
+
+echo Moving into the RetroCraft Folder
+
+if exist "..\..\RetroCraft\RetroCraft.jar" (
+    del "..\..\RetroCraft\RetroCraft.jar"
+)
+
+move RetroCraft.jar ..\..\RetroCraft
 cd ..\..\
